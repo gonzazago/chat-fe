ab7f7f5b0c123515ca36b4a41d74c5fdccd0fbd2
Gonzalo Zago <gonzalo.zago@pedidosya.com>
2024-08-29 19:54:46 -0300

    test

--- src/app/(hiring)/page.tsx
+++ src/app/(hiring)/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
         <>
             <div className="flex items-center justify-center min-h-screen background-custom">
                 <div className="relative z-10 flex flex-col items-center">
-                    <h1 className="mb-4 text-3xl font-bold text-white">Encuentra tu candidato</h1>
+                    <h1 className="mb-4 text-3xl font-bold text-white">Encuentra tu candidatoo</h1>
                     <div className="flex items-center p-4 bg-white rounded-lg shadow-lg">
                         <Search />
                     </div>
